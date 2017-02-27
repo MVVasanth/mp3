6a826feca27c95f2d5394b75169a893684d343f6
Vasanth <vasanth.manivijayarani@aspiresys.com>
2017-02-27 17:36:49 +0530

    my first successful commit

---
+++ Song/Song/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Song
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Application to start\n");
+            //Console.WriteLine("Enter Root Path:\n");
+            //var rootPath = Console.ReadLine();
+            //for testting
+            var rootPath = @"C:\mine\Watch";
+            Start(rootPath);
+        }
+
+        public static void Start(string rootPath)
+        {
+            var fileUtil = new mp3Utility();
+            //Console.WriteLine("Enter file: {0}", rootPath);
+
+            fileUtil.Find(rootPath);
+        }
+    }
+}
---
+++ Song/Song/mp3Utility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Song
+{
+    public class mp3Utility
+    {
+        public string RootPath { get; set; }
+
+        public void Find(string filePath)
+        {
+            //loop through all folders.
+            var folderNames = Directory.GetFiles(filePath);
+            //var currentDirectory = 
+        }
+
+        public void Move(string sourcePath, string destPath)
+        {
+
+        }
+
+        public void Delete(string filePath)
+        {
+
+        }
+    }
+}
